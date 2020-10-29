1b6da7a957062f280f84489524825afad0c7f23c
Liseidy Bueno <liseidybueno@gmail.com>
2020-10-28 21:09:54 -0400

    First Commit

---
+++ app.js
@@ -0,0 +1,18 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.us(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+
+app.listen(3000, function() {
+  console.log("Server started successfully.");
+});
